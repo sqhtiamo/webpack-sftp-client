b245cb325c457b191a7451f1de82f78a8fdd181c
Ken Chiang <kensomanpow@gmail.com>
2016-10-13 21:27:48 +0800

    upgrade scp2 version to v0.5.0 to support port parsing, and update original port variable assignment

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ var client = require('scp2');
 
 function WebpackSftpClient(options) {
     this.options = options;
+    var self = this;
+
+    var remotePath = self.options.remotePath;
+    var path = self.options.path;
+    var username = self.options.username;
+    var host = self.options.host;
+    var password = self.options.password;
+    var port = self.options.port || '22';
+
+        console.log(username + ':' + password + '@' + host + ':' + port + ':' + remotePath);
 }
 
 WebpackSftpClient.prototype.apply = function(compiler) {
@@ -24,9 +34,10 @@ WebpackSftpClient.prototype.apply = function(compiler) {
         var username = self.options.username;
         var host = self.options.host;
         var password = self.options.password;
+        var port = self.options.port || '22';
 
         client.scp(self.options.path,
-            username + ':' + password + '@' + host + ':' + remotePath,
+            username + ':' + password + '@' + host + ':' + port + ':' + remotePath,
             function (err) {
                 if (err) {
                     console.log(err);
@@ -38,6 +49,15 @@ WebpackSftpClient.prototype.apply = function(compiler) {
 
     });
 };
+
+new WebpackSftpClient({
+    port: '55',
+    host: 'exmaple.com',
+    username: 'root',
+    password: 'password',
+    path: './build/',
+    remotePath: '/data/website/demo/'
+});
 
 
 module.exports = WebpackSftpClient;
