246887e5a7d047d7c00dd5c20760f4eb0d94ff3b
ZYH <sqhtiamo@163.com>
2017-01-16 22:29:09 +0800

    feat - add new parameters

--- index.js
+++ index.js
@@ -7,17 +7,21 @@
 
 'use strict';
 
-var client = require('scp2');
+var ClientLib = require('scp2');
+
+var glob = require('glob');
+
+var client = new ClientLib.Client();
 
 function WebpackSftpClient(options) {
     this.options = options;
 }
 
-WebpackSftpClient.prototype.apply = function(compiler) {
+WebpackSftpClient.prototype.apply = function (compiler) {
 
     var self = this;
 
-    compiler.plugin('after-emit', function(compilation) {
+    compiler.plugin('done', function (compilation) {
 
         var remotePath = self.options.remotePath;
         var path = self.options.path;
@@ -26,18 +30,40 @@ WebpackSftpClient.prototype.apply = function(compiler) {
         var password = self.options.password;
         var port = self.options.port || '22';
 
-        client.scp(self.options.path,
-            username + ':' + password + '@' + host + ':' + port + ':' + remotePath,
+        client.on('connect', function () {
+            // console.log('connected');
+        });
+        client.on('ready', function () {
+            // console.log('ready');
+        });
+        client.on('transfer', function (buf, up, total) {
+            up = up + 1;
+            console.log(buf)
+            // console.log('transfer ' + up  + '/' + total + ' data', up < total);
+        });
+        client.on('read', function (p) {
+            // console.log('read ' + p);
+        });
+        client.on('close', function () {
+            // console.log('close');
+        });
+        client.on('end', function () {
+            // console.log('end');
+        });
+
+        ClientLib.scp(path,
+            username + ':' + password + '@' + host + ':' + port + ':' + remotePath, client,
             function (err) {
                 if (err) {
                     console.log(err);
                 }
                 else {
-                    console.log('Transfer with SFTP Completd!')
+                    console.log('Transfer with SFTP Completed');
                 }
-        });
-
+            }
+        );
     });
 };
+
 
 module.exports = WebpackSftpClient;
